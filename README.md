de90c8ccd86a54590cf7453bbfaa2f301d3d3669
Camilo <camil0_andr3s@hotmail.com>
2025-02-15 11:23:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# riverpod_map_romanjust
+
+A new Flutter project.
+# riverpod_map
---
+++ android/app/src/main/kotlin/com/example/riverpod_map_romanjust/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.riverpod_map_romanjust
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
